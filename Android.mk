9fdc42bbe713ea961ebab7ed6a2aa86e5f5bdf45
Erik Castricum <git@cas-online.nl>
2020-04-18 17:02:20 +0200

    Revert "Settings: remove obsolete choose utils"
    This reverts commit 59e0ee67cbd2e35dd802984682bfca0d19179868.

--- Android.mk
+++ Android.mk
@@ -34,7 +34,8 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.recyclerview_recyclerview \
     com.google.android.material_material \
     setupcompat \
-    setupdesign
+    setupdesign \
+    choose-util
 
 LOCAL_JAVA_LIBRARIES := \
     telephony-common \
